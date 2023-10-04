cf77d5cbbea0de98b35690f8972049f26cebbe50
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-10-05 00:59:03 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Program1
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+   
+        char chs = sc.next().charAt(0);
+        int in1 = chs;
+        System.out.println(in1);
+
+    }
+}
